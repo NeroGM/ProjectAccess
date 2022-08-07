5918a133b80eb730134b4a0e6fcb811a92b5c348
NeroGM <Nero.GM@protonmail.com>
2022-08-07 05:40:59 +0200

    Github.ps1 functions alphabetical sorting

--- src/Github.ps1
+++ src/Github.ps1
@@ -1,38 +1,3 @@
-function Set-GithubAuth {
-    [CmdletBinding()]
-    [OutputType([System.Void])]
-    param(
-        [Parameter(Mandatory)]
-        [string]
-        $Token
-    )
-
-    process {
-        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseDeclaredVarsMoreThanAssignments','',Scope='Global')]
-        $global:GH_TOKEN = ConvertTo-SecureString $Token -AsPlainText -Force
-    }
-}
-
-function Request-GithubUserData {
-    [CmdletBinding()]
-    [OutputType([Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject])]
-    param()
-
-    process {
-        $splat = @{
-            'Uri' = 'https://api.github.com/user'
-            'Authentication' = 'OAuth'
-            'Token' = $global:GH_TOKEN
-            'Headers' = @{
-                'Accept' = 'application/vnd.github+json'
-            }
-        }
-        $userData = Invoke-RestMethod @splat
-    
-        $userData
-    }
-}
-
 function Register-ProjectItem {
     [CmdletBinding()]
     [OutputType([Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject])]
@@ -84,5 +49,40 @@ function Register-ProjectItem {
         }
         
         Write-Output $res
+    }
+}
+
+function Request-GithubUserData {
+    [CmdletBinding()]
+    [OutputType([Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject])]
+    param()
+
+    process {
+        $splat = @{
+            'Uri' = 'https://api.github.com/user'
+            'Authentication' = 'OAuth'
+            'Token' = $global:GH_TOKEN
+            'Headers' = @{
+                'Accept' = 'application/vnd.github+json'
+            }
+        }
+        $userData = Invoke-RestMethod @splat
+    
+        $userData
+    }
+}
+
+function Set-GithubAuth {
+    [CmdletBinding()]
+    [OutputType([System.Void])]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $Token
+    )
+
+    process {
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseDeclaredVarsMoreThanAssignments','',Scope='Global')]
+        $global:GH_TOKEN = ConvertTo-SecureString $Token -AsPlainText -Force
     }
 }
